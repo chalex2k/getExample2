94625e9f3b15a1bd3202888022f05ab87ef01f8e
aychirkin <aychirkin@avito.ru>
2024-03-14 14:32:19 +0300

    добавил вывод qqqqqq23

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("qqqqqqqqq");
         System.out.println("qqqqqqqqq2");
+        System.out.println("qqqqqqqqq3");
+        System.out.println("qqqqqqqqq4");
     }
 }
